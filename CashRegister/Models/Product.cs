95700fb3d012e01f47fdf1cf8707f8eae670c9c9
Niva Rajkarnikar <nrajkarnikar@ipaymentinc.com>
2018-11-24 19:14:34 -0800

    removed unused files

--- CashRegister/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CashRegister.Models
-{
-    public class Product
-    {
-        public int ProductID { get; set; }
-        public string ProductName { get; set; }
-        public decimal UnitPrice { get; set; }       
-    }
-}
